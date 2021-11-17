6ea543aab69440e050c69517b482f6eddbcc669b
Alex-Nagy <nagyal3x@gmail.com>
2021-11-17 10:35:30 +0100

    a

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const generateArray = function(amount) {
     // amount = amountConverted
     
     amount = parseInt(amount)
+    // console.log(amount)
 
-    if (typeof amount === "number") {
-        console.log("amount is a number")
+    if (!Number.isNaN(amount)) {
+        // console.log("amount is a number")
     } else {
-        console.log("amount is not a number")
+        // console.log("amount is not a number")
         returnArray.push("error");
     }
 
@@ -22,14 +23,22 @@ const generateArray = function(amount) {
 }
 
 const loadEvent  = function() {
-
     console.log('az oldal betoltodott')
-    console.log(generateArray(100))
+/* 
+    console.log(generateArray('100'))
     console.log(generateArray(15))
     console.log(generateArray("kutya"))
     console.log(generateArray([2]))
-}
+*/
+    const root = document.getElementById("root")
+    const list = generateArray(4)
 
+    if (list[0] !== "error") {
+        for (const item of list) {
+            root.insertAdjacentHTML("beforeend", `<div> ${item} </div>`);
+        }
+    }
+}
 window.addEventListener("load", loadEvent)
 
 /*
@@ -38,3 +47,12 @@ window.addEventListener("load", loadEvent)
     })
 
  */
+
+
+/* 
+
+todo: hf uj function ahanyat generálunk ugy irja ki a szamokat pl 200 -> 001 002 003 ... 200
+
+todo: css a szamok kartya formamban jelenjenek meg
+
+*/
