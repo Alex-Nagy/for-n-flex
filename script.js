ef054f9af9faa17581ec7ff28b815e491b3df8d7
Alex-Nagy <nagyal3x@gmail.com>
2021-11-17 09:51:26 +0100

    ja

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-const pageLoad  = () => {
+const generateArray = function(amount) {
+    let returnArray = [];
 
+    // let amountConverted = parseInt(amount)
+    // amount = amountConverted
+    
+    amount = parseInt(amount)
+
+    if (typeof amount === "number") {
+        console.log("amount is a number")
+    } else {
+        console.log("amount is not a number")
+        returnArray.push("error");
+    }
+
+    for (let i = 0; i < amount; i++) {
+        returnArray.push(`${i}`);
+        
+    }
+
+    return returnArray    
 }
 
-window.addEventListener("load", pageLoad)
+const loadEvent  = function() {
+
+    console.log('az oldal betoltodott')
+    console.log(generateArray(100))
+    console.log(generateArray(15))
+    console.log(generateArray("kutya"))
+    console.log(generateArray([2]))
+}
+
+window.addEventListener("load", loadEvent)
+
+/*
+    window.addEventListener("load", () => {
+       * ez is teljesen jo
+    })
+
+ */
