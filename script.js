d5b41c1df4dcc86e72c239dec2ff00d49c8baa21
Alex-Nagy <nagyal3x@gmail.com>
2021-11-18 08:09:12 +0100

    card

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const generateArray = function(amount) {
-    let returnArray = [];
-
-    // let amountConverted = parseInt(amount)
-    // amount = amountConverted
-    
-    amount = parseInt(amount)
-    // console.log(amount)
-
-    if (!Number.isNaN(amount)) {
-        // console.log("amount is a number")
-    } else {
-        // console.log("amount is not a number")
-        returnArray.push("error");
-    }
-
-    for (let i = 0; i < amount; i++) {
-        returnArray.push(`${i}`);
-        
-    }
-
-    return returnArray    
-}
-
 const generateCards = (num) => {
     num = parseInt(num)
 
@@ -42,20 +18,6 @@ const generateCards = (num) => {
 
 const loadEvent  = function() {
     console.log('az oldal betoltodott')
-/* 
-    console.log(generateArray('100'))
-    console.log(generateArray(15))
-    console.log(generateArray("kutya"))
-    console.log(generateArray([2]))
-    const root = document.getElementById("root")
-    const list = generateArray(4)
-    
-    if (list[0] !== "error") {
-        for (const item of list) {
-            root.insertAdjacentHTML("beforeend", `<div> ${item} </div>`);
-        }
-    }
- */
 
     const root = document.getElementById("root")
     const list = generateCards(22)
@@ -65,14 +27,6 @@ const loadEvent  = function() {
     }
 }
 window.addEventListener("load", loadEvent)
-
-/*
-    window.addEventListener("load", () => {
-       * ez is teljesen jo
-    })
-
- */
-
 
 /* 
 
